82f695b0ed30450733088a051fafc3c97e2e7f57
Guido Zuccarelli <guido.zuccarelli@kognitiv.com>
2018-12-03 13:23:46 -0300

    Allow moving back to the hidden month

--- src/DateRangePicker.jsx
+++ src/DateRangePicker.jsx
@@ -402,7 +402,7 @@ const DateRangePicker = createClass({
   },
 
   canMoveBack() {
-    if (this.getMonthDate().subtract(1, 'days').isBefore(this.state.enabledRange.start)) {
+    if (this.getMonthDate().isBefore(this.state.enabledRange.start)) {
       return false;
     }
     return true;
